c9c3343fb9002c2ce48d9c48e72cde3b4cb77faa
Geonna Denola <geonnadenola@gmail.com>
2025-08-28 02:23:07 +0800

    Add files via upload

--- MoralMatrix/admin/dashboard.php
+++ MoralMatrix/admin/dashboard.php
@@ -10,6 +10,22 @@ session_start();
     <title>Document</title>
 </head>
 <body>
+    <header>
+        <div class="nav-left">
+            <a href="">moral matrix</a>
+        </div>
+
+        <div class="nav-right">
+            <form action="/MoralMatrix/logout.php" method="post">
+                <button type="submit" name="logout">Logout</button>
+            </form>
+        </div>
+    </header>
+
     <p>WELCOME ADMIN</p>
+
+    <a href="add_users.php">
+        <button>Add Users</button>
+    </a>
 </body>
 </html>
