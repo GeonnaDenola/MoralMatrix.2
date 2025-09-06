d6225ac970d4f492198a735b3cadd49d42b9ff85
Geonna Denola <geonnadenola@gmail.com>
2025-09-07 05:21:43 +0800

    Add files via upload

--- MoralMatrix/ccdu/dashboard.php
+++ MoralMatrix/ccdu/dashboard.php
@@ -22,9 +22,56 @@ include '../includes/header.php';
 
     <div class="right-container">
         <h2>Dashboard</h2>
-        <input type="text" id="search" placeholder="Search..." onkey="">
+        <input type="text" id="search" placeholder="Search..." onkeyup="">
+
+        <div class="cardContainer" id="studentContainer">
+            Loading...
+        </div>
     </div>
 
-    
+<script>
+    function loadStudents(){
+        fetch("get_students.php")
+        .then(response => response.json())
+        .then(data =>{
+            const container = document.getElementById("studentContainer");
+            container.innerHTML = ""; // Clear Loading
+
+            if (data.length === 0){
+                container.innerHTML = "<p>No student records found.</p>";
+                return;
+            }
+
+            data.forEach(student =>{
+                const card = document.createElement("div");
+                card.classList.add("card");
+
+                card.onclick = () => viewStudent(student.record_id);
+
+                card.innerHTML = `
+                    <div class="left">
+                        <img src="${student.photo ? '../admin/uploads/' + student.photo : 'placeholder.png'}" alt="Photo">
+                        <div class="info">
+                        <strong>${student.student_id}</strong><br>
+                        ${student.last_name}, ${student.first_name} ${student.middle_name}<br>
+                        ${student.institute}<br>
+                        ${student.course} - ${student.level}${student.section}
+                    </div>
+                </div>
+            `;
+            container.appendChild(card);
+            });
+        })
+        .catch(error => {
+            document.getElementById("studentContainer").innerHTML = "<p>Error Loading Data.</p>";
+            console.error("Error fetching student data: ", error);
+        });
+    }
+
+    function viewStudent(id){
+        window.location.href="view_student.php?id=" + id;
+    }
+    loadStudents();
+</script> 
 </body>
 </html>
