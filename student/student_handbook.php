ced45de22db57609cb1f2ceea487191f6f71ee3b
Geonna Denola <geonnadenola@gmail.com>
2025-10-02 16:07:32 +0800

    student side

---
+++ student/student_handbook.php
@@ -0,0 +1,18 @@
+<?php
+include '../includes/header.php';
+include '../config.php';
+
+include 'menu_buttons.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
