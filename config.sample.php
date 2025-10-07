efa21edf1ba4cc20dbc75fc0326203379ac8591a
Geonna Denola <geonnadenola@gmail.com>
2025-10-07 20:54:09 +0800

    Use untracked config.php; add config.sample.php; ignore secrets

---
+++ config.sample.php
@@ -0,0 +1,9 @@
+<?php
+// Copy to config.php and fill real values. DO NOT COMMIT config.php.
+return [
+  'twilio' => [
+    'sid'   => 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'token' => 'your_auth_token_here',
+    'from'  => '+1XXXXXXXXXX'
+  ],
+];
